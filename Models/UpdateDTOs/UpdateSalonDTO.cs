736ba447175b630cbf81c515018f0485885ca736
Remus Ene <Ionut-Remus.Ene@endava.com>
2023-02-24 17:40:22 +0200

    DTO's updated for salon

--- Models/UpdateDTOs/UpdateSalonDTO.cs
+++ Models/UpdateDTOs/UpdateSalonDTO.cs
@@ -2,6 +2,10 @@
 {
     public class UpdateSalonDTO
     {
+        public int Floor { get; set; }
         public bool Available { get; set; }
+        public int NumberOfBeds { get; set; }
+        public int SalonNumber { get; set; }
+        public Guid SectionId { get; set; }
     }
 }
