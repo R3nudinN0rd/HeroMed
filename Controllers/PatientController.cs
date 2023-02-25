36024682195f9494a3038e0fe921d6bd9c9a216a
Remus Ene <r3nudinn0rd@outlook.com>
2023-02-25 15:49:44 +0200

    Test for merging

--- Controllers/PatientController.cs
+++ Controllers/PatientController.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HeroMed_API.Controllers
 {
+    /*tEST for merging*/
     [ApiController]
     [Route("/api/patient")]
     [EnableCors("AllowOrigins")]
